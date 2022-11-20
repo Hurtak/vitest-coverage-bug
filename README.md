8ea0a4548f02b197a1460b0e679c6b387917f4a1
Hurtak <petr.hurtak@gmail.com>
2022-11-20 18:28:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitest-coverage-bug
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitest-dev-vitest-7tthne)
---
+++ packages/app/src/folder/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+import { add } from './index';
+
+test('add', () => {
+  expect(add(1, 2)).toBe(3);
+});
